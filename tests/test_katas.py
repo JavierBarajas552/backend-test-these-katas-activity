3b31441f6aa47182b253faba1f3a7a91c144f8b8
Javier Barajas <Bishamon552@gmail.com>
2020-06-25 14:51:26 -0500

    added unittest

--- tests/test_katas.py
+++ tests/test_katas.py
@@ -1,22 +1,38 @@
 import unittest
 import katas
+import math
+import random
 
 
 class TestKatas(unittest.TestCase):
     def test_add(self):
-        self.fail("TODO: Write add unit test")
+        first = int(math.floor(random.random()*100))
+        second = int(math.floor(random.random()*100))
+        test = katas.add(first, second)
+        self.assertEqual(first + second, test)
 
     def test_multiply(self):
-        self.fail("TODO: Write multiply unit test")
+        first = int(math.floor(random.random()*100))
+        second = int(math.floor(random.random()*100))
+        test = katas.multiply(first, second)
+        self.assertEqual(first * second, test)
 
     def test_power(self):
-        self.fail("TODO: Write power unit test")
+        first = int(math.floor(random.random()*100))
+        second = int(math.floor(random.random()*100))
+        test = katas.power(first, second)
+        self.assertEqual(first ** second, test)
 
     def test_factorial(self):
-        self.fail("TODO: Write factorial unit test")
+        first = int(math.floor(random.random()*100))
+        test = katas.factorial(first)
+        self.assertEqual(math.factorial(first), test)
 
     def test_fibonacci(self):
-        self.fail("TODO: Write fibonacci unit test")
+        fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+        first = int(math.floor(random.random()*10))
+        test = katas.fibonacci(first+1)
+        self.assertEqual(fib[first], test)
 
 
 if __name__ == '__main__':
